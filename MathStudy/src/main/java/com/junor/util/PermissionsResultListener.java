5294457bedf86042d6493fbeb55d66f429272d75
wangtao <wangtao@duia.com>
2018-07-25 10:39:46 +0800

    删除不需要的类

---
+++ MathStudy/src/main/java/com/junor/util/PermissionsResultListener.java
@@ -0,0 +1,14 @@
+package com.junor.util;
+
+/**
+ * author : taowang
+ * date :2018/7/21
+ * description:
+ **/
+public interface PermissionsResultListener {
+    //成功
+    void onSuccessful(int[] grantResults);
+
+    //失败
+    void onFailure();
+}
